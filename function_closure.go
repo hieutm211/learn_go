59f01a10aca2012bd8cb6aff9fea074f7514a5c0
Hieu Tran <hieutm211@gmail.com>
2018-10-30 17:03:38 +0700

    practice about function closure

---
+++ function_closure.go
@@ -0,0 +1,22 @@
+
+package main
+
+import "fmt"
+
+func fibonacci() func() int {
+	a := 0
+	b := 1
+	return func() int {
+		b = a+b
+		a = b-a
+		return b-a
+	}
+}
+
+func main() {
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
+}
+
