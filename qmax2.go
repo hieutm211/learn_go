fb40bc9aadba13e2fed4d4a63826a363994165f1
Hieu Tran <hieutm211@gmail.com>
2018-11-06 15:57:16 +0700

    speed up with bufio

--- qmax2.go
+++ qmax2.go
@@ -3,6 +3,9 @@ package main
 
 import (
     "fmt"
+    "os"
+    "bufio"
+    "io"
 )
 
 var n int
@@ -61,22 +64,41 @@ func (node *segmentTree) Get(k, l, r, u, v int) int {
     return max(node.Get(2*k, l, mid, u, v), node.Get(2*k+1, mid+1, r, u, v))
 }
 
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+	return ""
+    }
+    return string(str)
+}
+
 func main(){
+    reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024) 
+    writer := bufio.NewWriter(os.Stdout)
+
     var m, c, u, v, x int
     var st segmentTree
+    var temp string
 
-    fmt.Scanln(&n, &m)
+    temp = readLine(reader)
+    fmt.Sscanf(temp, "%d%d", &n, &m)
 
     st.Init(n)
 
     for ; m > 0; m-- {
-	fmt.Scan(&c)
+	temp = readLine(reader)
+
+	fmt.Sscanf(temp, "%d%d%d%d", &c, &u, &v, &x)
 	if c == 0 {
-	    fmt.Scanln(&u, &v, &x)
 	    st.Update(1, 1, n, u, v, x)
 	} else {
-	    fmt.Scanln(&u, &v)
-	    fmt.Println(st.Get(1, 1, n, u, v))
+	    fmt.Fprintf(writer, "%d\n", st.Get(1, 1, n, u, v))
+	}
+
+	if m%1000 == 0 {
+	    writer.Flush()
 	}
     }
+    writer.Flush()
 }
+
